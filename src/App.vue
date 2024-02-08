fdef8edf743a00bc61b16c281ba7bd9a4c36e66f
Miguel Alejandro <miguel.santiesteban.aguiar@gmail.com>
2024-02-08 15:28:33 +0000

    ESlint

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
-
 <script setup lang="ts">
-  import NavBar from './components/navbar/NavBar.vue'
-  import Carrusel from './components/Carrusel.vue'
-  import Contenedor from './components/Contenedor.vue'
-  import AppFooter from './components/Footer.vue'
+import NavBar from "./components/navbar/NavBar.vue";
+import Carrusel from "./components/AppCarrusel.vue";
+import Contenedor from "./components/AppContenedor.vue";
+import AppFooter from "./components/AppFooter.vue";
+asdasda;
 </script>
 
 <template>
-    <NavBar />
-    <Carrusel />
-    <Contenedor /> 
-    <AppFooter />
+  <NavBar />
+  <Carrusel />
+  <Contenedor />
+  <AppFooter />
 </template>
 
-<style scoped>
-  
-</style>
+<style scoped></style>
